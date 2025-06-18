824254b34e586e6e4f061d8384feb648a1562e79
lookmori <58687559+lookmori@users.noreply.github.com>
2025-06-18 09:29:08 +0800

    调整图像问题

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['s.coze.cn'],
+  },
 };
 
 export default nextConfig;
